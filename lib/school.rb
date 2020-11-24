db9aa479da7c58c48dce9683b9724b92a2e83e31
Alec Scully <ScullyAlec@gmail.com>
2020-11-24 16:01:32 -0500

    commit tree 977e6832392174c7f54216f2629a11a7446d0555

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
-# code here!
+require 'pry'
+class School
+    attr_reader :name
+    attr_accessor :roster, :add_student
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+    # binding.pry
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        # sorted = @roster[grade].sort
+        # sorted = @roster.sort
+        sorted = @roster.sort_by { |grade, name| grade }
+        # binding.pry
+    end
+end
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 10)
+school.add_student("Jeff", 9)
+school.add_student("Alec", 9)
+school.add_student("bob", 10)
+school.add_student("zach", 10)
+school.add_student("Al", 10)
+
+# binding.pry
